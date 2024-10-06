578de4efda89a7d735a83a4f758dae9b195b0c5b
Kunal prajapati <106148593+Kunalprajapati1@users.noreply.github.com>
2024-10-06 13:34:54 +0530

    database added

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -7,15 +7,26 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCrsbARBvoDDi1o4AmPVv_3Iu5I1qi4BQw",
+//   authDomain: "unity-of-youth.firebaseapp.com",
+//   projectId: "unity-of-youth",
+//   storageBucket: "unity-of-youth.appspot.com",
+//   messagingSenderId: "594050608974",
+//   appId: "1:594050608974:web:19b78677c182a1d684781f",
+//   measurementId: "G-T496JJ5HE1"
+// };
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCrsbARBvoDDi1o4AmPVv_3Iu5I1qi4BQw",
-  authDomain: "unity-of-youth.firebaseapp.com",
-  projectId: "unity-of-youth",
-  storageBucket: "unity-of-youth.appspot.com",
-  messagingSenderId: "594050608974",
-  appId: "1:594050608974:web:19b78677c182a1d684781f",
-  measurementId: "G-T496JJ5HE1"
+  apiKey: "AIzaSyBYgSvfd37aem3qRVQSgoruzQAjwu_7Hik",
+  authDomain: "unity-of-youth-foundation.firebaseapp.com",
+  projectId: "unity-of-youth-foundation",
+  storageBucket: "unity-of-youth-foundation.appspot.com",
+  messagingSenderId: "770621220210",
+  appId: "1:770621220210:web:e5d7c4f55d003c4f5829eb",
+  measurementId: "G-G3B75XEQ1P"
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
